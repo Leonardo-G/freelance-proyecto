e7ae95587857202a66b8f86700a33ada97c202b3
leonardog9823 <guanuco43@gmail.com>
2022-02-27 23:59:11 -0300

    tecnologias con imagenes completadas

--- src/components/Tecnologias.js
+++ src/components/Tecnologias.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Tecnologia } from './Tecnologia';
 
 import btn_left from "../assets/arrow-left-circle.svg";
@@ -11,6 +11,23 @@ import ide4 from "../assets/ides/vsc.svg";
 import ide5 from "../assets/ides/ij-ide.svg";
 
 import lenguaje1 from "../assets/lenguajes/c.png";
+import lenguaje2 from "../assets/lenguajes/c++.png";
+import lenguaje3 from "../assets/lenguajes/ce.png";
+import lenguaje4 from "../assets/lenguajes/java.png";
+import lenguaje5 from "../assets/lenguajes/phyton.png";
+import lenguaje6 from "../assets/lenguajes/php.png";
+import lenguaje7 from "../assets/lenguajes/ts.png";
+import lenguaje8 from "../assets/lenguajes/javascript.png";
+
+import framework1 from "../assets/framworks/codeigniter.png";
+import framework2 from "../assets/framworks/dj.png";
+import framework3 from "../assets/framworks/react.png";
+import framework4 from "../assets/framworks/net.png";
+import framework5 from "../assets/framworks/laravel.png";
+import framework6 from "../assets/framworks/angular.png";
+
+import mobile1 from "../assets/frameworks-mobiles/flutter.png";
+import mobile2 from "../assets/frameworks-mobiles/xamarin.png";
 
 import "./Tecnologias.scss";
 
@@ -23,17 +40,49 @@ const tecnologiasArray = [
         { tipo: "IDE", src: ide5, alt: "ide5" },
     ],
     [
-        { tipo: "lenguaje", src: lenguaje1, alt: "lenguaje1" },
+        { tipo: "lenguaje", src: lenguaje1, alt: "lenguaje c" },
+        { tipo: "lenguaje", src: lenguaje2, alt: "lenguaje c++" },
+        { tipo: "lenguaje", src: lenguaje3, alt: "lenguaje c#" },
+        { tipo: "lenguaje", src: lenguaje4, alt: "lenguaje java" },
+        { tipo: "lenguaje", src: lenguaje5, alt: "lenguaje phyton" },
+        { tipo: "lenguaje", src: lenguaje6, alt: "lenguaje php" },
+        { tipo: "lenguaje", src: lenguaje7, alt: "lenguaje ts" },
+        { tipo: "lenguaje", src: lenguaje8, alt: "lenguaje javascript" },
     ],
     [
-        { tipo: "lenguaje2", src: lenguaje1, alt: "lenguaje1" },
-        { tipo: "lenguaje2", src: ide5, alt: "ide5" },
+        { tipo: "framework", src: framework1, alt: "framework CodeIgniter" },
+        { tipo: "framework", src: framework2, alt: "framework dj" },
+        { tipo: "framework", src: framework3, alt: "framework react" },
+        { tipo: "framework", src: framework4, alt: "framework asp.net" },
+        { tipo: "framework", src: framework5, alt: "framework laravel" },
+        { tipo: "framework", src: framework6, alt: "framework angular" },
+    ],
+    [
+        { tipo: "mobile", src: mobile1, alt: "mobile Flutter" },
+        { tipo: "mobile", src: mobile2, alt: "mobile Xamarin" }
     ],
 ]
 
+const tipos = ["IDE", "Lenguajes de programación", "Frameworks", "Frameworks mobile"]
+
 export const Tecnologias = () => {
 
+    const [numeroArray, setNumeroArray] = useState(1);
+    const [tipoActual, setTipoActual] = useState(tipos[0])
+
+    useEffect(() => {
+        const tipoSeleccionado = tipos.filter( (tipo, idx) => (idx + 1) === numeroArray )[0]
+        console.log(tipoSeleccionado)
+        setTipoActual(tipoSeleccionado)
+    }, [numeroArray])
+
     const handleClickLeft = () => {
+
+        if( numeroArray === 1 ){
+            numeroArray(tipos.length);
+            return;
+        }
+
         const ref = document.querySelector(".flex-contenedor");
         const idx = ref.children.length - 1;
         const ultimoElemento = ref.children[idx];
@@ -51,6 +100,14 @@ export const Tecnologias = () => {
     } 
 
     const handleClickRight = () => {
+
+        if( numeroArray >= Number(tipos.length) ){
+            setNumeroArray(1);
+            return
+        }
+
+        setNumeroArray( numeroArray + 1)
+
         const ref = document.querySelector(".flex-contenedor");
         console.log(ref);
 
@@ -78,7 +135,7 @@ export const Tecnologias = () => {
             <h2 className='titulo'>Tecnologías que uso</h2>
             <div className='caja'>
                 <div className='caja__borde'>
-                    <p>IDE</p>
+                    <p>{ tipoActual }</p>
                 </div>
                 <div 
                     className='caja__btn-left'
