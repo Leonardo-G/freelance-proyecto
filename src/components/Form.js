ffaad8da356fcd11685f37636cff72f8e57e28e6
leonardog9823 <guanuco43@gmail.com>
2022-03-03 23:26:28 -0300

    animacion final en formulario

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import "../assets/mundo.png";
 
@@ -7,9 +7,23 @@ import "./Form.scss";
 import videoForm from "../assets/correoenviado.mp4"
 
 export const Form = () => {
+
+    const [error, setError] = useState({
+        error: false,
+        msg: ""
+    })
+
+    const [inputsValues, setInputsValues] = useState({
+        nombre: "",
+        correo: "",
+        asunto: "",
+        mensaje: ""
+    })
+
     const enviarCorreo = async (e) => {
 
         e.preventDefault();
+
 
         const bloque1 = document.querySelector("#bloque1");
         const bloque2 = document.querySelector("#bloque2");
@@ -48,6 +62,19 @@ export const Form = () => {
         // }
     }
 
+    const handleReady = () => {
+
+        const bloque3 = document.querySelector("#bloque3");
+        const bloque4 = document.querySelector("#bloque4");
+        const bloque5 = document.querySelector("#bloque5");
+        const bloque6 = document.querySelector("#bloque6");
+
+        bloque3.classList.remove("fondo-activo");
+        bloque4.style.display = "none";
+        bloque5.style.display = "none";
+        bloque6.style.display = "flex"
+    }
+
     return (
         <section className='contacto'>
             <h2 className='titulo'>Contacto</h2>
@@ -61,17 +88,21 @@ export const Form = () => {
                     <input 
                         type="text"
                         placeholder='Nombre'
+                        name='nombre'
                     />
                     <input 
                         type="email"
                         placeholder='Email'
+                        name='correo'
                     />
                     <input 
                         type="text"
                         placeholder='Asunto'
+                        name='asunto'
                     />
                     <textarea
                         placeholder='Tú mensaje...'
+                        name='mensaje'
                     ></textarea>
                     <input 
                         className='btn btn-submit'
@@ -89,9 +120,18 @@ export const Form = () => {
                     ></video>
                 </div>
                 <div id="bloque3" className='enviado-fondo'>
-                    <div className='enviado-contenedor'>
+                    <div id='bloque5' className='enviado-contenedor'>
                         <h3 className='activo-titulo'>¡Tu mensaje ha sido enviado!</h3>
-                        <div className='btn'>Aceptar</div>
+                        <div 
+                            className='btn'
+                            onClick={ handleReady }    
+                        >Aceptar</div>
+                    </div>
+                    <div id='bloque6' className='enviado-info'>
+                        <div className='info-container'>
+                            <p>A la brevedad te estaré contactando.</p>
+                        </div>
+                        <p className='info-texto'>¡Muchas gracias!</p>
                     </div>
                 </div>
             </div>
